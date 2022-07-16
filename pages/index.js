a812a7334b4e89316e08d191f0789109cc6a21b6
Binay Timilsina <binayprogramming@gmail.com>
2022-07-16 17:30:54 +0200

    Added Danelfin icon as well as modified the header

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,14 @@ export default function Home() {
     <div>
       <Head>
         <title>{title}</title>
-        <meta name="description" content="This is the test for Danelfin Frontend Developer" />
+        <meta name="description" content="Pick the best stocks and optimize your portfolio with the power of our Artificial Intelligence. Get unbiased and unique insights, 
+        and make smart data-driven investment decisions." />
         <link rel="icon" href="/logoDanelfin.ico" />
       </Head>
 
-      <main>
-        <h1>Yo mama</h1>
+      <main style={{ display: "flex", flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: '30px' }}>
+        <h1>Invest With the Odds in Your Favor</h1>
+        <p>Artificial Intelligence to pick the best stocks and optimize your portfolios. Get unique insights and make smart data-driven investment decisions.</p>
       </main>
     </div>
   )
